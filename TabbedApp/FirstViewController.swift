d9938acb55ea8a25b2b92e39aecc0c1188c37f4d
Eduardo TC <eduar2tc@gmail.com>
2020-03-18 00:58:04 -0600

    Initial Commit

---
+++ TabbedApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabbedApp
+//
+//  Created by Edu on 18/03/20.
+//  Copyright © 2020 Edu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabbedApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabbedApp
+//
+//  Created by Edu on 18/03/20.
+//  Copyright © 2020 Edu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
